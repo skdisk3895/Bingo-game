0aa1d786e34107f8afe29170249f6616d02b0925
HyeBin <skdisk7368@naver.com>
2021-08-12 13:45:02 +0900

    Create Bingo component & UI

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,45 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div class="reset-btn">
+      <button>Reset</button>
+    </div>
+    <Board />
+    <Board />
+  </div>
 </template>
 
 <script>
-export default {
+import Board from "@/components/Board.vue";
 
-}
+export default {
+  components: {
+    Board,
+  },
+};
 </script>
 
-<style>
+<style scoped>
+#app {
+  height: 1200px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  overflow: hidden;
+}
 
+#app .reset-btn {
+  position: absolute;
+  top: 90px;
+}
+
+#app .reset-btn button {
+  border: none;
+  background-color: #2643e9;
+  color: #ffffff;
+  width: 230px;
+  height: 80px;
+  border-radius: 40px;
+  font-size: 20px;
+  cursor: pointer;
+}
 </style>
--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,13 +1,57 @@
 <template>
-  <div></div>
+  <div class="board">
+    <div class="board-number" v-for="(number, idx) in bingoBoard" :key="idx">{{ number }}</div>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      bingoBoard: [],
+    };
+  },
+  created() {
+    this.shakeBingo();
+  },
+  methods: {
+    // 빙고판을 랜덤으로 섞는다.
+    shakeBingo: function () {
+      const arr = [];
+      for (let i = 1; i <= 25; i++) arr.push(i);
+      arr.sort(() => Math.random() - 0.5);
+      console.log(arr);
+      this.bingoBoard = arr;
+    },
+  },
+};
 </script>
 
-<style>
+<style scoped>
+* {
+  box-sizing: border-box;
+}
 
+.board {
+  width: 750px;
+  border: 1px solid rgb(180, 180, 180);
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.board .board-number {
+  border: 1px solid rgb(180, 180, 180);
+  flex: 0 0 20%;
+  height: 150px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 30px;
+  cursor: pointer;
+}
+
+.board .board-number.active {
+  color: white;
+  background-color: #d91ae0;
+}
 </style>
