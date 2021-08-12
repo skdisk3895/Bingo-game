85b026ceeb231e80c29bf18fd882022cd6d56315
양혜빈 <skdisk7368@naver.com>
2021-08-12 13:19:01 +0900

    init bingo

---
+++ bingo-game/src/components/Board.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      board: []
+    }
+  },
+  created() {
+    arr = []
+    for (let i = 1; i <= 25; i++)
+      arr.push(i);
+    this.board = arr
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
